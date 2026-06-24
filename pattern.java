3a57b4eb94a95026e0d819fac3dc875788b5d915
Saransh-raj <saranshraj011@gmail.com>
2026-06-24 22:17:31 +0530

    adding reverse pyramid

--- pattern.java
+++ pattern.java
@@ -100,6 +100,26 @@ public class pattern{
 
         System.out.println(); // for looking good
 
+
+            //  * * * * * * * 
+            //    * * * * * 
+            //      * * * 
+            //        * 
+
+        for (int i = 0; i < 4; i++) {
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            // stars
+            for (int k = 0; k < (2 * (4 - i) - 1); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(); // for looking good
+
         
         sc.close();
     }
